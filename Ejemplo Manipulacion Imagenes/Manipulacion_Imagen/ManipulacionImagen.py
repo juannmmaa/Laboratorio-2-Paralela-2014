a3c487f4a57d6f5d94e0bb0f5be9376443a7ba7a
Diego <diogo.again@icloud.com>
2014-10-15 21:27:09 -0300

    cambie la funcion convertirImgNegativa para que se visualize que se esta trabajando pixel a pixel

--- Ejemplo Manipulacion Imagenes/Manipulacion_Imagen/ManipulacionImagen.py
+++ Ejemplo Manipulacion Imagenes/Manipulacion_Imagen/ManipulacionImagen.py
@@ -13,15 +13,17 @@ def convertirImgMatrixRGB(img):
 # procedimiento : multiplica por base 255 cada casilla de la matriz RGB para convertir la imagen en negativo
 def convertirImgNegativo(img):
     arrImg=convertirImgMatrixRGB(img)
-    imgNegativa=Image.fromarray(arrImg*255)
+    for i in range(img.size[1]):
+        for j in range(img.size[0]):
+            arrImg[i][j]=arrImg[i][j]*255
+    imgNegativa=Image.fromarray(arrImg)
     return imgNegativa
+
 
 
 def main():
     img=Image.open("ViendoFacebook.png")
     imgNegativa=convertirImgNegativo(img)
-    imgNegativa.save("output.png")    #guarda la imagen negativo
-
-
+    imgNegativa.save("output3.png")    #guarda la imagen negativo
 
 main()
