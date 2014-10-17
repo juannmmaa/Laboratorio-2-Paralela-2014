ea8d38dfe418eb1f4e428f9ddc2d0793b9acc084
FcoHernan <FranciscoHRF@gmail.com>
2014-10-17 00:40:00 -0300

    RGB Listo falta Evaluacion

--- Produccion/Francisco Ramirez/RGB.py
+++ Produccion/Francisco Ramirez/RGB.py
@@ -13,26 +13,20 @@ def convertirImgMatrixRGB(img):
 # y cada suma se divide en dos, obteniendo un promedio
 def mezclarRGB(img,r,g,b):
     arrImg=convertirImgMatrixRGB(img)
-    #print arrImg
     for i in range(img.size[1]):
         for j in range(img.size[0]):
-            #print str(i),",",str(j)
-            #print arrImg[i][j]
             arrImg[i][j][0] = (arrImg[i][j][0]+r)/2
             arrImg[i][j][1] = (arrImg[i][j][1]+g)/2
             arrImg[i][j][2] = (arrImg[i][j][2]+b)/2
-            #print arrImg[i][j]
-    imgNegativa=Image.fromarray(arrImg)
-    return imgNegativa
-
-
+    imgRGB=Image.fromarray(arrImg)
+    return imgRGB
 
 def main():
     img=Image.open("ViendoFacebook.png")
-    r=0
-    g=0
-    b=0
-    imgNegativa=mezclarRGB(img,r,g,b)
-    imgNegativa.save("output3.png")    #guarda la imagen negativo
+    r=50
+    g=160
+    b=320
+    imgRGB=mezclarRGB(img,r,g,b)
+    imgRGB.save("output3.png")    #guarda la imagen RGB
 
 main()
