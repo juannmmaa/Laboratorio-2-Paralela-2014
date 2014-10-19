fa0728932f063e0942e29a58f3c26771b5f77f9c
jonathan <jonathan-leon@hotmail.com>
2014-10-19 16:20:51 -0300

    sumaFoto.py y mascaradeCapa.py
    Se actualiza suma de fotos segun lo pedido y se sube la primera version
    de mascaras de capa.

---
+++ Produccion/Jonathan Leon/Mascaras de Capa/mascara.py
@@ -0,0 +1,34 @@
+__author__ = 'jonathan'
+
+import numpy as np
+from PIL import Image,ImageOps
+
+def convertirImgMatrixRGB(img):
+    return np.array(img.convert("RGB"))
+
+#Genera un valor entre 0 y 1 a partir de un pixel en escala de grises
+#Mientras mas alto sea , el valor devuelto será mas cercano a 1 (opacidad maxima)
+def obtenerAlfa(pixel):
+    return pixel[0]/255
+
+#Pega una imagen encima de un fondo, usando una mascara para generar las zonas transparentes de esta.
+def componerImagen(fondo,img,mascara):
+    arrFondo=convertirImgMatrixRGB(fondo)
+    arrImg=convertirImgMatrixRGB(img)
+    arrMascara=convertirImgMatrixRGB(mascara)
+    for i in range(fondo.size[1]):
+        for j in range(fondo.size[0]):
+            alfa=obtenerAlfa(arrMascara[i][j])  #el alfa se usa para representar un nivel de transparencia de una imagen dada
+            sumaPixel= (arrFondo[i][j]*(1-alfa))+(arrImg[i][j]*(alfa))#se suman los pixeles multiplicando la imagen por el alfa y el fondo por el complemento(ver informe)
+            arrFondo[i][j]=sumaPixel
+    imgCompuesta=Image.fromarray(arrFondo)
+    return imgCompuesta
+
+def main():
+    fondo=Image.open("galaxia.jpg")
+    img=Image.open("barco.jpg")
+    mascara=Image.open("alfaBarco.jpg")
+    imagenCompuesta=componerImagen(fondo,img,mascara)
+    imagenCompuesta.save("output.png")
+
+main()
--- Produccion/Jonathan Leon/Suma de fotos(pixel a pixel)/sumaFotos.py
+++ Produccion/Jonathan Leon/Suma de fotos(pixel a pixel)/sumaFotos.py
@@ -4,19 +4,19 @@ __author__ = 'jonathan'
 import numpy as np
 from PIL import Image
 
-#Nivel de transparencia de imagen 1
-ALFA=0.3
+#Nivel de transparencia de imagen 1, para que queden igualmete mezcladas se debe usar un alfa=0.5
+ALFA=0.5
 
 def convertirImgMatrixRGB(img):
     return np.array(img.convert("RGB"))
 
-#suma las imagenes pixel a pixel, usando un valor alfa para sacar una proporcion de mezcla entre imagenes
+#mezcla 2 imagenes en una,el alfa es usado para definir el nivel de transparencia de las imagenes.
 def sumarImagenes(img1,img2,alfa):
     arrImg1=convertirImgMatrixRGB(img1)
     arrImg2=convertirImgMatrixRGB(img2)
     for i in range(img1.size[1]):
         for j in range(img1.size[0]):
-            sumaPixel= (arrImg1[i][j]*(alfa))+(arrImg2[i][j]*(1-alfa))
+            sumaPixel= (arrImg1[i][j]*(1-alfa))+(arrImg2[i][j]*(alfa))
             arrImg1[i][j]=sumaPixel
     imgSuma=Image.fromarray(arrImg1)
     return imgSuma
