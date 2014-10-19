d0698616e4df948257c58b7cad88e4b1feea2437
Jonathan León <jonathan-leon@hotmail.com>
2014-10-19 13:42:29 -0300

    Update sumaFoto.py
    cambiando script para que trabaje pixel a pixel.

--- Produccion/Jonathan Leon/Suma de fotos/sumaFoto.py
+++ Produccion/Jonathan Leon/Suma de fotos/sumaFoto.py
@@ -1,30 +1,32 @@
-__author__ = 'jonathan leon'
+__author__ = 'jonathan'
 
-"""
-sumaFoto.py
-
-Entrada : 2 urls con imagenes, un valor float entre 0 y 1 
-
-Salida  : Una imagen mezclada a partir de las originales
-
-"""
 
 import numpy as np
-import os.path
 from PIL import Image
 
-def sumaFoto(url1,url2,alfa):   
-    if os.path.exists(url1)==False: #comprobamos existencia de archivo
-        return "No se encontro el archivo %s" %(url1)
-     if os.path.exists(url2)==False:  #comprobamos existencia de archivo
-        return "No se encontro el archivo %s" %(url2)    
-    if alfa>1 or <0:                  #comprobamos valor del alfa  
-        return "Valor alfa invalido, debe ser entre 0 y 1."
-    
-    img1=Image.open(url1)
-    img2=Image.open(url2)
-    
-    return resultado=Image.blend(img1,img2,alfa)  #sumamos las imagenes  
-       
+#Nivel de transparencia de imagen 1
+ALFA=0.3
+
+def convertirImgMatrixRGB(img):
+    return np.array(img.convert("RGB"))
+
+#suma las imagenes pixel a pixel, usando un valor alfa para sacar una proporcion de mezcla entre imagenes
+def sumarImagenes(img1,img2,alfa):
+    arrImg1=convertirImgMatrixRGB(img1)
+    arrImg2=convertirImgMatrixRGB(img2)
+    for i in range(img1.size[1]):
+        for j in range(img1.size[0]):
+            sumaPixel= (arrImg1[i][j]*(alfa))+(arrImg2[i][j]*(1-alfa))
+            arrImg1[i][j]=sumaPixel
+    imgSuma=Image.fromarray(arrImg1)
+    return imgSuma
+
+def main():
+    img1=Image.open("galaxia.jpg")
+    img2=Image.open("barco.jpg")
+    imagenSumada=sumarImagenes(img1,img2,ALFA)
+    imagenSumada.save("resultado.jpg")
+
+main()
 
 
