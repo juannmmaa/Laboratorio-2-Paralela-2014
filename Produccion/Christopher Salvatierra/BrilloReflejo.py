95a0dba30dacf8d6a4b5e3aef6f1014663dd5f2c
Chris2141 <c.salvatierra.icci@gmail.com>
2014-10-22 16:23:44 -0300

    Update BrilloReflejo.py

--- Produccion/Christopher Salvatierra/BrilloReflejo.py
+++ Produccion/Christopher Salvatierra/BrilloReflejo.py
@@ -17,7 +17,7 @@ def convertirImgMatrixRGB(img):
 
 #Trabajando en como matriz siempre
 def aplicarBrillo(img,factor):
-    arrImg=convertirImgMatrixRGB(img)
+    arrImg=convertirImgMatrixRGB(img) #pasamos la imagen a una matriz para aplicar operaciones matematicas
     #si el factor es menor a cero la imagen se oscurece (rango 0 a -1)
     if factor <0:#para oscurecer la imagen
         for i in range(img.size[1]):
@@ -30,12 +30,12 @@ def aplicarBrillo(img,factor):
             for j in range(img.size[0]):
                 brillo= lambda x: x+ (255-x)*factor
                 arrImg[i][j]=brillo(arrImg[i][j])
-    imgBrillante=Image.fromarray(arrImg)
+    imgBrillante=Image.fromarray(arrImg) #Se convierte la matriz a imagen nuevamente para devolverla
     return imgBrillante
 
 def main():
     img=Image.open("imagenMuestra2.jpg")
-    print"Ingrese factor"
+    print"Ingrese factor (debe ingresar valor decimal, entre -1 y 0 para oscurecer y entre 0 y 1 para aclarar"
     factor=input()
     imgBrillo = aplicarBrillo(img,factor)
     imgBrillo.save("resultado1.jpg")
