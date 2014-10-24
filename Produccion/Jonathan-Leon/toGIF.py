d02a1d513dc0c14c3c81a1cbc839c836e29a47c8
Jonathan León <jonathan-leon@hotmail.com>
2014-10-24 00:19:10 -0300

    Create toGIF.py
    avance de convertidor de gif

---
+++ Produccion/Jonathan-Leon/toGIF.py
@@ -0,0 +1,16 @@
+"""
+generadorGIF.py
+
+Este programa corre un comando de la API ImageMagick
+
+para instalar:
+  apt-get install imagemagick
+
+"""
+from os import system
+
+nombreSalida="animacion.gif"
+delay=10
+filepath="*jpg"
+#el delay representa la pausa entre una imagen y otra y loop 0 especifica que el gif se repite en un bucle. *jpg tomara todos los archivos *jpg (para este caso, puede ser cualquier formato de imagen)
+system('convert -delay %d -loop 0 %s %s ' % (delay,filepath,nombreSalida))
