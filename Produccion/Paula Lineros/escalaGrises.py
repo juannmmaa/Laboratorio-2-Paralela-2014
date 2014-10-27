a817219127017ea4a148e26dc39059323d0ee8a5
Paula Lineros <pau.lokilla@gmail.com>
2014-10-27 14:35:57 -0300

    escala de grises

---
+++ Produccion/Paula Lineros/escalaGrises.py
@@ -0,0 +1,27 @@
+import ImageFilter
+import numpy as np
+import time
+from numpy import array, shape, reshape, zeros, transpose
+from PIL import Image, ImageChops, ImageOps
+
+# convierte una imagen tipo Imagen (de la libreria PIL) en una matriz(ETD) con la informacion RGB de la imagen
+def convertirImgMatrixRGB(img):
+    return np.array(img.convert("RGB"))
+    
+def escalaGrises(img):
+    n = img.size[0]
+    m = img.size[1]
+    for i in range(n):
+        for j in range(m):
+            # Obtenemos los colores RGB pixel por pixel
+            r, g, b = img.getpixel((i, j))
+            # Cambiamos el RGB del pixel
+            img.putpixel((i, j), ((r+g+b)/3, (r+g+b)/3, (r+g+b)/3))
+    return img
+ 
+def main():
+    img=Image.open("imagennormal.png") #abre imagen
+    imgGris = escalaGrises(img)
+    imgGris.save("imagenescaladegris.png") #guarda la imagen en escala de grises
+    
+main()
