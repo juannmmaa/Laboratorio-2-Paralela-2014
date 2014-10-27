932392514c08e8a074bb8f6029f30079d8a74bea
Chris2141 <c.salvatierra.icci@gmail.com>
2014-10-26 22:51:38 -0300

    Update avanceReflejo.py

--- Produccion/Christopher Salvatierra/avanceReflejo.py
+++ Produccion/Christopher Salvatierra/avanceReflejo.py
@@ -12,20 +12,31 @@ def convertirImgMatrixRGB(img):
 
 
 def aplicarReflejo(img):
+    inv = img #se hace una copia de la imagen
+    inv = inv.rotate(180) #se gira
+    copiaInvertida = convertirImgMatrixRGB(inv) #se convierten en matrices ambas imagenes
     imgMatriz = convertirImgMatrixRGB(img)
     filas = img.size[1]
     columnas = img.size[0]
-    filTotal = filas
-    copia = imgMatriz
-    print copia
-    for i in range (filas):
-        for j in range(columnas):
-            fila = copia[i][j] #tomar la primera fila de la matriz y se copia en un arreglo
 
-    return 0
+    '''filTotal = filas
+    copia = imgMatriz #una matriz de copia para mantener la original
+    print "LA MATRIZ ORIGINAL"
+    print copia
+    for i in range (0,filas):
+        for j in range(0,columnas):
+            fila = copia[i][j] #tomar la primera fila de la matriz y se copia en un arreglo
+            #copia[i][j] = copia[(filas-1) -i][(columnas-1) -j]
+            copia[(filas-1)-i][(columnas-1)-j] = fila #copia la fila obtenida al final de la matriz
+    print "LA MATRIZ INVERTIDA"
+    '''
+    arreglo = np.vstack((imgMatriz,copiaInvertida))
+    #print copiaInvertida
+    imgInvertida = Image.fromarray(arreglo)
+    return imgInvertida
 
 def main():
     img=Image.open("imagenMuestra2.jpg")
     imgReflejo = aplicarReflejo(img)
-
+    imgReflejo.save("reflejo.jpg")
 main()
