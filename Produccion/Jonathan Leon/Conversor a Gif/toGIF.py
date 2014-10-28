376c92c5974fbeac5fe8a735c00dc465804c637e
Jonathan León <jonathan-leon@hotmail.com>
2014-10-28 08:31:05 -0300

    Update toGIF.py
    se añade calculo de tiempo.

--- Produccion/Jonathan Leon/Conversor a Gif/toGIF.py
+++ Produccion/Jonathan Leon/Conversor a Gif/toGIF.py
@@ -9,9 +9,20 @@ ejecutar en un terminal
 
 """
 from os import system
+import time
 
-nombreSalida="animacion.gif"
-delay=10
-filepath="imagenes/*png"
-#el delay representa la pausa entre una imagen y otra y loop 0 especifica que el gif se repite en un bucle. *jpg tomara todos los archivos *jpg (para este caso, puede ser cualquier formato de imagen)
-system('convert -delay %d -loop 0 %s %s ' % (delay,filepath,nombreSalida))
+def toGif(fp,delay,output):
+  #el delay representa la pausa entre una imagen y otra y loop 0 especifica que el gif se repite en un bucle. *jpg tomara todos los archivos *jpg (para este caso, puede ser cualquier formato de imagen)
+  system('convert -delay %d -loop 0 %s %s ' % (delay,filepath,nombreSalida))
+
+def main():
+  starting_point=time.time()
+  nombreSalida="animacion.gif"
+  delay=10
+  filepath="imagenes/*png"
+  toGif(filepath,delay,nombreSalida)
+  elapsed_time=time.time()-starting_point
+  print ""
+  print "Serial Time [seconds]: " + str(elapsed_time)
+
+
