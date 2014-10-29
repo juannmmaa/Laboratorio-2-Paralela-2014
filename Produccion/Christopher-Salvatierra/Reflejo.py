677fcd9c777715c5571abefaac2092ed192ff7a1
Chris2141 <c.salvatierra.icci@gmail.com>
2014-10-29 13:32:24 -0300

    Update Reflejo.py
    código corregido para hacer correctamente el reflejo

--- Produccion/Christopher-Salvatierra/Reflejo.py
+++ Produccion/Christopher-Salvatierra/Reflejo.py
@@ -5,34 +5,62 @@ __author__ = 'Christopher Salvatierra L.'
 import numpy as np
 from PIL import Image
 from PIL import ImageEnhance
-import time #Libreria
 
 # convierte una imagen tipo Imagen (de la libreria PIL) en una matriz(ETD) con la informacion RGB de la imagen
 def convertirImgMatrixRGB(img):
     return np.array(img.convert("RGB"))
 
 
+def rotar90(img):
+    arrImg = convertirImgMatrixRGB(img)
+    n = img.size[0]
+    m = img.size[1]
+    final = np.array(Image.new("RGB",(m,n)))
+    for i in range(n): #fila
+        for j in range(m): #columna
+            final[i][j] = arrImg[::, ((i*-1)-1)][j]
+    imgColor = Image.fromarray(final)
+    return imgColor
+
+def rotar180(img):
+    return rotar90(rotar90(img)) #debido a que el algoritmo de cambio de posicion es el mism
+
 def aplicarReflejo(img):
     inv = img #se hace una copia de la imagen
-    inv = inv.rotate(180) #se gira
+    inv = rotar180(inv) #se gira
+
     copiaInvertida = convertirImgMatrixRGB(inv) #se convierten en matrices ambas imagenes
+
     imgMatriz = convertirImgMatrixRGB(img)
-    
+    filas = img.size[1]
+    columnas = img.size[0]
+
+    #Es una idea
+
+    print "MATRIZ ORIGINAL"
+    print imgMatriz
+    for i in range (0,filas):
+        for j in range(0,columnas):
+            aux = imgMatriz[i][j]
+            copiaInvertida[filas-1][j] = aux
+        filas=filas-1
+    print "MATRIZ MODIFICADA"
+    print copiaInvertida
+
     arreglo = np.vstack((imgMatriz,copiaInvertida))
-    #print copiaInvertida
+
     imgInvertida = Image.fromarray(arreglo)
     return imgInvertida
 
 def main():
-    starting_point=time.time() #Donde quiere empezar a calcular el tiempo
-    img=Image.open("1.jpg")
+    img=Image.open("imagenMuestra2.jpg")
     imgReflejo = aplicarReflejo(img)
     imgReflejo.save("reflejo.jpg")
-    elapsed_time=time.time()-starting_point #calculo
-    print ""
-    print "Serial Time [seconds]: " + str(elapsed_time) #segundos
 
-
-
+import time #Libreria
+starting_point=time.time() #Donde quiere empezar a calcular el tiempo
 main()
+elapsed_time=time.time()-starting_point #calculo
+print ""
+print "Serial Time [seconds]: " + str(elapsed_time) #segundos
 
