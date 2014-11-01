686bcb17bdb883258cf5df7d0e777701a2451f81
FcoHernan <FranciscoHRF@gmail.com>
2014-11-01 01:59:33 -0300

    arreglo pequeño para evitar errores de ruta

--- Produccion/Francisco Ramirez/Estrategia Paralelo/cortarFoto.py
+++ Produccion/Francisco Ramirez/Estrategia Paralelo/cortarFoto.py
@@ -9,15 +9,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-def cortarImagen(ruta, x, y):
-    im = Image.open(ruta+'1.jpg')
+def cortarImagen( x, y):
+    im = Image.open('1.jpg')
     data= np.array(im.convert("RGB"))
     region = im.crop((0, 0,632 ,236 ))
     region.save("new1", "JPEG")
     region = im.crop((0, 236,632 ,474 ))
     region.save("new2", "JPEG")
 
-ruta='/home/francisco/Documentos/Laboratorio-2-Paralela-2014/Produccion/Francisco Ramirez/Estrategia Paralelo/'
-cortarImagen(ruta,0,236)
+#ruta='/home/francisco/Documentos/Laboratorio-2-Paralela-2014/Produccion/Francisco Ramirez/Estrategia Paralelo/'
+#cortarImagen(ruta,0,236)
+cortarImagen(0,236)
 
 
