49eb836272f720867050f2bb611b63a67ecc66af
Chris2141 <c.salvatierra.icci@gmail.com>
2014-11-06 12:16:56 -0300

    Update ReflejoParalelo.py
    optimizado

--- Produccion/Christopher-Salvatierra/ReflejoParalelo.py
+++ Produccion/Christopher-Salvatierra/ReflejoParalelo.py
@@ -1,79 +1,20 @@
 __author__ = 'Christopher Salvatierra L.'
-# -*- coding: utf-8 -*-
 
-import numpy as np
-from StringIO import StringIO
-import time
+
+
 from mpi4py import MPI
-from pylab import *
-import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
+from PIL import Image,ImageChops
+import StringIO
 
-starting_point=time.time()
+comm = MPI.COMM_WORLD  # comunicador entre dos procesadores #
 
-#Explicacion paralelo
-#Para cada procesador ingresado por el terminal, ya sea "np"=4 o "np"=12 va a funcionar de la misma forma,
-#la distribución para que los "np" procesadores, será dividir la cantidad total de datos
-#por la cantidad de procesado, dejandolos de forma pareja. En el caso que sobren será asignado para
-#el ultimo procesador. A continuación se describiran brevemente que realizarán cada función
+rank = comm.rank     # id procesador actual #
+size = comm.size     # tamano procesador #
 
 
-comm = MPI.COMM_WORLD  # comunicador entre dos procesadores
-rank = comm.rank     # id procesador actual
-size = comm.size     # cantidad de procesadores a usar
-
-# En esta funcion se mandara a cada procesador la cantidad de datos con la que trabajaran
-# Si sobran datos para que sean parejos, se le asignara al ultimo procesador
-def distribuirEnP(size,altura):
-    if (rank == 0):
-        cuoc = (altura) / (size-2) #c : cuociente
-        rest = (altura) % (size-2) #r : resto
-        conta = 0
-        for p in range(size-2):
-            if (p+1) != (size-2):
-                conta = conta + cuoc
-                comm.send(conta, dest = p+2)
-            else:
-                conta = conta+cuoc+rest
-                comm.send(conta, dest = p+2)
-
-# El procesador 0 recibirá las cantidad de datos con la que trabajará
-# cada procesador para devolver los indice i y j hasta donde operará
-def buscarRangoFinal(base,altura):
-    if rank==2:
-        p=2
-        conta=0
-        rangos_end=[]
-        valor=0
-        for i in range(altura):
-            for j in range(base):
-                if(valor==0):
-                    valor=comm.recv(source=p)
-                if conta==valor:
-                    rangos_end = rangos_end + [i,j]
-                    comm.send(rangos_end,dest=p)
-                    rangos_end=[]
-                    p = p + 1
-                    conta = conta + 1
-                    valor=0
-                else:
-                    conta = conta + 1
-
-# convierte una imagen tipo Imagen (de la libreria PIL) en una matriz(ETD) con la informacion RGB de la imagen
 def convertirImgMatrixRGB(img):
     return np.array(img.convert("RGB"))
-
-def cortarImagen(x, y,base):
-    #print "hol"
-    im = Image.open('1.jpg')
-    region = im.crop((0, x, base, y))
-    region.save("cut"+str(rank)+".jpg")
-    factor = 0.5
-    #data= np.array(region.convert("RGB"))
-    #mezclarRGB(region,r,g,b)
-    #aplicarBrillo(im,factor)
-    aplicarReflejo(im)
 
 def rotar90(img):
     arrImg = convertirImgMatrixRGB(img)
@@ -89,7 +30,8 @@ def rotar90(img):
 def rotar180(img):
     return rotar90(rotar90(img)) #debido a que el algoritmo de cambio de posicion es el mism
 
-def aplicarReflejo(img):
+def aplicarReflejo(arreglo):
+    img = Image.fromarray(arreglo)
     inv = img #se hace una copia de la imagen
     inv = rotar180(inv) #se gira
 
@@ -111,61 +53,59 @@ def aplicarReflejo(img):
     #print "MATRIZ MODIFICADA"
     #print copiaInvertida
 
-    arreglo = np.vstack((imgMatriz,copiaInvertida))
+    #arreglo = np.vstack((imgMatriz,copiaInvertida))
+    arreglo = copiaInvertida
 
-    imgInvertida = Image.fromarray(arreglo)
-    #return imgInvertida
-    imgInvertida.save("end.jpg")
+    #imgInvertida = Image.fromarray(arreglo)
+    return arreglo
 
+#funcion que recibe la ruta de imagen y distribuye los trozos horizontales a cada procesador excepto el cero
+def divisionTareaImagen(ruta):
+    img=Image.open(ruta)
+    imgSize=img.size
+    largo=imgSize[1]
+    ancho=imgSize[0]
+    tamanoParte=largo/(size-1)  #(size-1) es para no incluir el procesador cero
+    xInicio=0
+    yInicio=0
+    tamPar=tamanoParte
+    for i in range(1,size):
+        parteImgEnvio=img.crop((xInicio,yInicio,ancho,tamPar))
+        tamPar=tamPar+tamanoParte
+        yInicio=yInicio+tamanoParte
+        rutaSalida="photoCut"+str(i)+".png"
+        parteImgEnvio.save(rutaSalida)
+        arrImg=convertirImgMatrixRGB(parteImgEnvio)
+        comm.send(arrImg,dest=i)
 
-#-------------MAIN---------------------
-
-# Se sobre entiende que los delimitadores son espacios
-data = Image.open("1.jpg")
-data=convertirImgMatrixRGB(data)
-altura = data.shape[0]
-base = data.shape[1]
-
-# El procesador 0 estará a cargo de mandar la cantidad de datos
-# para cada procesador
-if rank == 0:
-    print ""
-    print "altura,base: ",altura,base
-    distribuirEnP(size,altura)
-
-if rank >= 2:
-    # Recibe la cantidad de datos en cada procesador
-    fin = comm.recv(source=0)
-    #fin = fin - 1
-    #envia los datos con que finalizan al siguiente procesador para que lo usen como inicio
-    if size != 3:
-        if rank == 2:
-            ini = 0
-            i = 1
-            if size > 0:
-                comm.send(fin, dest=3)
-    else:
-        ini=0
-        comm.send(fin,dest=2)
-    # Hasta los p procesadores
-    if size != 3:
-        if rank !=2:
-            ini=comm.recv(source=rank-1)
-            if (rank+1)<size:
-                comm.send(fin,dest=rank+1)
-    else:
-        ini=comm.recv(source=2)
-    print "rank ",rank,", ini,fin :",ini,",",fin
-
-    #r,g,b componentes del color a mezclar en decimales
-    # 255,0,0 rojo
+def main():
+    if rank==0:
+        ruta="imagenMuestra.jpg"
+        divisionTareaImagen(ruta)
+    if rank!=0:
+        arrTrabajo=comm.recv(source=0)    #cada procesador recibe un arreglo RGB que contiene un trozo horizontal de la imagen
+        arrImgSalida=aplicarReflejo(arrTrabajo)    #enviar el arreglo RGB a transformarlo en arreglo negativo de la imagen
+        comm.send(arrImgSalida,dest=0)
+    if rank==0 :       #recibe los arreglos y los junta uno abajo del otro
+        for i in range(1,size):
+            if i > 1:
+                construcImg = np.concatenate((comm.recv(source=i),construcImg))
 
 
-    cortarImagen(ini,fin,base)
+            if i == 1:
+                construcImg = comm.recv(source=i)
 
-if rank ==2:
-#    Calculo de tiempo
-    elapsed_time=time.time()-starting_point
-    elapsed_time_int = int(elapsed_time)
-    print "Parallel Time [seconds]: " + str(elapsed_time)
-    print ""
+        img = Image.open("imagenMuestra.jpg")
+        imgOriginal = convertirImgMatrixRGB(img)
+        arreglo = np.vstack((imgOriginal,construcImg))
+        imgContrucFinal=Image.fromarray(arreglo)
+        imgContrucFinal.save("resultado.jpg")
+
+
+
+import time #Libreria
+starting_point=time.time() #Donde quiere empezar a calcular el tiempo
+main()
+elapsed_time=time.time()-starting_point #calculo
+print ""
+print "Time [seconds]: " + str(elapsed_time) #segundos
